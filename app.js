9016e3b9433eada4a5ac990fb00601f5e634325f
wshert1688 <wsh1688@163.com>
2013-06-02 21:43:05 +0800

    some

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var express = require('express')
 
 var app = express();
 
+
+var MemStore = express.session.MemoryStore;
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -19,8 +21,33 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.cookieParser());
+app.use(express.session({secret: 'alessios', store: MemStore({
+    reapInterval: 60000 * 10
+})}));
+
+
+//设置脚手架
+app.use(function (req, res, next) {
+    var origRender = res.render;
+    res.render = function (view, locals, callback) {
+        if ('function' == typeof locals) {
+            callback = locals;
+            locals = undefined;
+        }
+        if (!locals) {
+            locals = {};
+        }
+        locals.req = req;
+        locals.session = req.session;
+        origRender.call(res, view, locals, callback);
+    };
+    next();
+});
+app.use(app.router);
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -29,7 +56,19 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/login', user.login);
+app.post('/login', user.postlogin);
+app.get('/reg', user.reg);
+app.post('/reg', user.postreg);
 
+
+app.get('/logout', function(req, res){
+    // destroy the user's session to log them out
+    // will be re-created next request
+    req.session.destroy(function(){
+        res.redirect('/');
+    });
+});
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,38 @@
 /*
  * GET users listing.
  */
-
+var db = require('../db');
 exports.list = function(req, res){
-  res.send("respond with a resource");
+    res.send("respond with a resource");
 };
+
+exports.login = function(req,res){
+    res.render('login', { title: "登录" });
+}
+
+exports.postlogin = function(req,res)
+{
+    db.validatelogin([req.body.username,req.body.password],function(err,rows){
+
+        if(rows!=undefined&&rows.length>0&&rows[0].name==req.body.username)
+        {
+            var user = rows[0];
+            req.session.id = user.id;
+            req.session.name = user.name;
+            res.redirect('/');
+        }
+        else
+        {
+            res.redirect('login');
+        }
+    })
+
+}
+exports.reg=function(req,res){
+    res.render('reg', { title: "注册" });
+}
+exports.postreg=function(req,res){
+
+    if(r)
+    res.render('reg', { title: "注册" });
+}
